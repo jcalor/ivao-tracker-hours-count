d593496bf3027c5243230eb2267c4e66d5053af2
Jorge Calo <jorgecalobergondo@gmail.com>
2025-02-17 17:38:55 +0100

    chore(front): add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { QueryClient, QueryClientProvider } from "react-query"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
-import "./index.css"
-import { AuthProvider } from "./modules/auth"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { AuthProvider } from "./modules/auth";
+import { Analytics } from "@vercel/analytics/react";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <AuthProvider>
@@ -16,4 +18,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         </AuthProvider>
       </BrowserRouter>
     </QueryClientProvider>
-)
+    <Analytics />
+  </>
+);
